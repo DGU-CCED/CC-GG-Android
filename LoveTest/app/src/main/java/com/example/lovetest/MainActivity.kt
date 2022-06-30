e33cd9435abe1da5b09cf2fe55dac96633e2d1e7
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-07-01 01:16:01 +0900

    [FEAT] Make Fragments of LoveTest App

--- LoveTest/app/src/main/java/com/example/lovetest/MainActivity.kt
+++ LoveTest/app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //navController = nav_host_fragment.findNavController()
+        //navController = findNavController(R.id.nav_host_fragment)
     }
 }
