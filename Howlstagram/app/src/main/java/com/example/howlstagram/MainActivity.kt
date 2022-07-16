84e09effcb81cade5b57d579886086c2a7d8c33f
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-07-17 00:41:59 +0900

    [FEAT] Make photo upload page

--- Howlstagram/app/src/main/java/com/example/howlstagram/MainActivity.kt
+++ Howlstagram/app/src/main/java/com/example/howlstagram/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.howlstagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.MenuItem
-import com.example.howlstagram.navigation.AlarmFragment
-import com.example.howlstagram.navigation.DetailViewFragment
-import com.example.howlstagram.navigation.GridFragment
-import com.example.howlstagram.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.howlstagram.navigation.*
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +32,10 @@ class MainActivity : AppCompatActivity(){
                     true
                 }
                 R.id.action_add_photo -> {
+                    if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        startActivity(Intent(this, AddPhotoActivity::class.java))
+                    }
+
                     true
                 }
                 R.id.action_favorite_alarm -> {
@@ -46,5 +53,9 @@ class MainActivity : AppCompatActivity(){
                 else -> false
             }
         }
+
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
+
+
 }
