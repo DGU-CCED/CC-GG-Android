7aa91b7476465f4b6cea469860e2188f650b8143
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-07-29 01:08:39 +0900

    [FEAT] Make alarm list page

--- Howlstagram/app/src/main/java/com/example/howlstagram/navigation/model/AlarmDTO.kt
+++ Howlstagram/app/src/main/java/com/example/howlstagram/navigation/model/AlarmDTO.kt
@@ -6,6 +6,10 @@ data class AlarmDTO(
     var destinationUid: String? = null,
     var userId: String? = null,
     var uid: String? = null,
+
+    //0: like alarm
+    //1: comment alarm
+    //2: follow alarm
     var kind: Int? = null,
     var message: String? = null,
     var timestamp: Long? = null
